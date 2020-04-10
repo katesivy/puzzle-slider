10f1f6d37ae8fa7c4da5941d523a3e2a1a0edca6
katesivy <58646769+katesivy@users.noreply.github.com>
2020-04-09 20:10:24 -0400

    tiles sort of switching

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -30,53 +30,40 @@ class Board extends React.Component {
     };
     this.setState({ tiles: tileArray });
   }
-  
+
   swapValues(id) {
     console.log('clicked', id)
-    // let blankObject = this.state.tiles.find(item, index);
-    //   if (item === index[0]) {
-    // console.log(item);
-    //   }
-    let currentClicked = this.state.tiles[id].location;
-    let clickedType = this.state.tiles[id].type;
-    let blankLocation = this.state.tiles[0].location;
-    let blankType = this.state.tiles[0].type;
-    blankType = 'blank';
+    let tempArray = this.state.tiles;
+    // let blankObject = this.state.tiles.find(i => i.id === 0);
+    // blankObject.className = 'blank';
+    let blankIndex = this.state.tiles.findIndex(i => i.location === 0);
+    let currentClicked = id;
+
     var clickedCol = currentClicked % 4;
     var clickedRow = parseInt(currentClicked / 4);
-    var blankCol = blankLocation % 4;
-    var blankRow = parseInt(blankLocation / 4);
+    var blankCol = blankIndex % 4;
+    var blankRow = parseInt(blankIndex / 4);
     var switchTiles = false;
 
-    // // evaluate if currentClicked is eligible to swap with blankLocation
-    if ((blankRow === clickedRow) && (Math.abs(clickedCol - blankCol) === 1)) {
+    if ((blankRow === clickedRow) && (Math.abs(clickedCol - blankCol)) === 1) {
       switchTiles = true;
     } else if
-      ((blankCol === clickedCol) && (Math.abs(clickedCol - blankCol) === 1)) {
+      ((blankCol === clickedCol) && (Math.abs(clickedRow - blankRow)) === 1) {
       switchTiles = true;
+    } else {
+      switchTiles = false;
     }
+
     if (switchTiles) {
       console.log('switch')
+      
+      let tempTile = tempArray[id].location;
+      tempArray[id].location = tempArray[blankIndex].location;
+      tempArray[blankIndex].location = tempTile;
+     
+       this.setState({ tiles: tempArray });
 
-      let tempTile = currentClicked;
-      currentClicked = this.state.tiles[0].location;
-      let newBlankLocation = tempTile;
-      let tempType = clickedType;
-      clickedType = blankType;
-      let newBlankType = tempType;
-
-      let tempArray = this.state.tiles;
-      tempArray[id].location = currentClicked;
-      tempArray[blankLocation] = newBlankLocation;
-      tempArray[id].type = clickedType;
-      tempArray[blankType] = newBlankType;
-
-      this.setState({ tiles: tempArray });
-
-      console.log({ currentClicked });
-      console.log({ tempTile });
-      console.log({ newBlankLocation })
-    }
+      }
   }
 
 
